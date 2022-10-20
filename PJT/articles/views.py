54eb74597f01c7c88ba3438cf0eb325da707b754
jaejae200 <kj31049@naver.com>
2022-10-20 16:19:48 +0900

    feat : add create

--- PJT/articles/views.py
+++ PJT/articles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
 
@@ -9,5 +10,22 @@ def index(request):
     context = {
         'articles': articles
     }
-    
+
     return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+            article.save()
+            return redirect('articles:index')
+
+    else:
+        form = ArticleForm()
+
+    context = {
+        'form' : form
+    }
+    return render(request, 'articles/form.html', context)
