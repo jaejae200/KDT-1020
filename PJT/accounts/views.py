ad020427ebea485ba0b738a68682c5de9e6bfa14
jaejae200 <kj31049@naver.com>
2022-10-20 15:58:20 +0900

    feat : add user detail page

--- PJT/accounts/views.py
+++ PJT/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.forms import AuthenticationForm
@@ -39,3 +40,10 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return redirect('articles:index')
+
+def detail(request, pk):
+    user = get_user_model.objects.get(pk=pk)
+    context = {
+        'user' : user
+    }
+    return render(request, 'accounts/detail.html', context)
